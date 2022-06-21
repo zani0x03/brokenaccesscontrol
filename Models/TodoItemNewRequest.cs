071d380e05502854b6d7dadb9d9019b437f55c44
Tiago Zaniquelli <1567810+zani0x03@users.noreply.github.com>
2022-06-21 15:44:41 -0300

    v3

--- Models/TodoItemNewRequest.cs
+++ Models/TodoItemNewRequest.cs
@@ -2,7 +2,6 @@ namespace brokenaccesscontrol.Models;
 
 public class TodoItemNewRequest
 {
-    public string Id {get; set;}
     public string Name { get; set; }
     public string Description { get; set; }
 }
--- Repositories/TodoItemRepository.cs
+++ Repositories/TodoItemRepository.cs
@@ -15,4 +15,39 @@ public static class TodoItemRepository
         var lstTodoItens = await conn.QueryAsync<TodoItem>(query);
         return lstTodoItens;
     }
+
+    public static async Task<TodoItem> GetToDoItem(int id)
+    {
+        var conn = SqliteConfigConnection.GetSQLiteConnection();
+        string query = "Select id, name, description from todoitems where id = @id";
+        var todoItem = await conn.QueryAsync<TodoItem>(query, new{
+            id = id
+        });
+        return todoItem.FirstOrDefault();
+    }    
+
+
+    public static async Task<bool> Delete(int id){
+
+        var conn = SqliteConfigConnection.GetSQLiteConnection(); 
+        var query = "delete from todoitems where id = @id";
+        var table = await conn.ExecuteAsync(query, new{
+            id = id
+        });        
+
+        return table > 0 ? true : false;        
+    }    
+
+    public static async Task<bool> Insert(TodoItemNewRequest todoItem, string userId){
+
+        var conn = SqliteConfigConnection.GetSQLiteConnection(); 
+        var query = "insert into todoitems(name, description, userId) values (@name, @description, @userId)";
+        var table = await conn.ExecuteAsync(query, new{
+            name = todoItem.Name,
+            description = todoItem.Description,
+            userId = userId
+        });        
+
+        return table > 0 ? true : false;        
+    }       
 }
